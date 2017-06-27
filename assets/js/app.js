4ed0e7cee433f945682dc8ea020332392f859a15
Ricardo Miranda <ricardomirannda@gmail.com>
2017-06-26 21:15:02 -0300

    Criacao das funcoes Cabecalho, Cartao Imagem e Validacao, alem de anexar as divs uma dentro da outra pra criar a hierarquia das informacoes

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,20 +7,24 @@ adicionarFilme.addEventListener("click", function(event){
 
 	event.preventDefault()
 
+	validaCampo();
+
+
   // Começamos aqui! Parte 1 : pegar os dados do form. Fiz uma função que retorna os dados do form 
   //em um objeto do javascript. Veja como fiz cada passo dessa função.
-  var dadosDoCartao = pegaDadosDoForm();
+  // var dadosDoCartao = pegaDadosDoForm();
 
   // Agora Parte 2:Criar o cartão. Agora vou criar uma função especialista em criar o HTML 
   //do cartão que comentei ali em cima, a partir dos dados do cartão que extraimos na variavel anterior
-  var novoCartaoHTML = criaCartao(dadosDoCartao);
+  // var novoCartaoHTML = criaCartao(dadosDoCartao);
 
   // Parte 3: Adicionar o cartão na página
   // Com o cartão criado, agora podemos adicionálo na div.row
   // Vamos criar uma função especialista nisso
-  adicionaCartaoNaPagina(novoCartaoHTML);
+  // adicionaCartaoNaPagina(novoCartaoHTML);
 
 
+  
 
 });
 
@@ -57,11 +61,20 @@ function pegaDadosDoForm(){
 
 function criaCartao(dados){
 
+	var cartaoCabecalho = cabecalhoDoCartao();
+
+	var cartaoImagem = cartaoDeImagem();
+
 	var corpoDoCartao = criaCorpoDoCartao();
 
 	var tituloDoCartao = criaTituloDoCartao(dados.titulo);
 
-	corpoDoCartao.appendChild(tituloDoCartao); 
+
+	cartaoCabecalho.appendChild(cartaoImagem); 
+	
+	corpoDoCartao.appendChild(cartaoCabecalho); 
+	
+	cartaoCabecalho.appendChild(tituloDoCartao); 
 
 	return corpoDoCartao;                                            
 
@@ -70,10 +83,25 @@ function criaCartao(dados){
 function criaCorpoDoCartao(){
 
 	var corpo = document.createElement("div"); 
-
 	corpo.classList.add("column");
 
 	return corpo;
+}
+
+
+function cabecalhoDoCartao(){
+	var cartaoCabecalho = document.createElement("div");
+	cartaoCabecalho.classList.add("card");
+
+	return cartaoCabecalho;
+}
+
+
+function cartaoDeImagem(){
+	var cartaoImagem = document.createElement("div");
+	cartaoImagem.classList.add("card-image");
+
+	return cartaoImagem;
 }
 
 
@@ -93,8 +121,30 @@ function criaTituloDoCartao(textoDoTitulo){
 
 function adicionaCartaoNaPagina(cartaoRecemCriado){
 
-	var conteudoTitulo = document.querySelector(".lista-filmes-series-animes");
+	var conteudoCartao = document.querySelector(".lista-filmes-series-animes");
 
-	conteudoTitulo.appendChild(cartaoRecemCriado)
+	conteudoCartao.appendChild(cartaoRecemCriado)
 }
+
+
+// VALIDAR O TITULO DO FORMULARIO 
+
+function validaCampo() {
+	var nomeDoFilme = document.querySelector(".nome-do-filme").value;
+	var mensagemDeErro = document.querySelector(".message-error");
+
+	if (nomeDoFilme == ""){
+		console.log("Você precisa informar o nome do filme");
+		mensagemDeErro.style.display = "block";
+	}else{
+		mensagemDeErro.style.display = "none";
+		var dadosDoCartao = pegaDadosDoForm();
+		var novoCartaoHTML = criaCartao(dadosDoCartao);
+		adicionaCartaoNaPagina(novoCartaoHTML);
+	}
+
+}
+
+
+
 
